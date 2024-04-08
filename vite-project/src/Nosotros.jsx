3f725e03812237c74ce9e068cf6964641c1c6179
Jiza <160131@mail.unacar.mx>
2024-04-08 12:46:16 -0600

    Ahora si Final

--- vite-project/src/Nosotros.jsx
+++ vite-project/src/Nosotros.jsx
@@ -44,12 +44,16 @@ function Nosotros() {
                 <div>
                     <p className='textobio'>
                         Estudiante de la Universidad Autonoma del Carmen, casi egresado de la carrera
-                        de Ing. en Diseño Multimedia. Tengo casi (no sé cuantos años llevas ) en el mundo de ilustración.
-                        No sé que cosas ponerte aquí asi que asi lo deje, ni moderrimo, pero intenta llenar este mismo 
-                        espacio de palabras, para que no se vea tan vacia la pagina jeje.
+                        de Ing. en Diseño Multimedia. Tengo aproximadamente 3 años y media en el mundo de ilustración.
+                        He tenido la oportunidad de trabajr con emprededores de Cd del Carmen, mas en el área de Diseño
+                        Grafico y en la publicidad para hacer que su empresa consiga un mejor posicionamiento o logre llegar a más 
+                        personas.
                         <p classname= 'textochico'>
-                        NO SE aqui escribe un poquito de lo que te gusta hacer idk amistad, es que no sepo, y ya, 
-                        solo es para dar info. 
+                        Desde hace 3 años como ya se mencionó comence a tomarme más enserio la ilustración y es algo en lo que me 
+                        gustaria enfocarme y dedicarme, sigo explorando intentando poder encontrar un estilo que me guste y se 
+                        cómodo para mi, algo tengo claro el realismo no me agrada, este es un proyecto que comence ya que me gustan
+                        mucho los juegos de mesa y es un mundo nuevo para mi, aproximadamente llevo 3 meses desarrollando este proyecto
+                        por mi cuenta, mecánicamente ya esta desarrollado solo faltan las ilustraciones de cada carta jugable.
                         </p>
                     </p>
                 </div>
